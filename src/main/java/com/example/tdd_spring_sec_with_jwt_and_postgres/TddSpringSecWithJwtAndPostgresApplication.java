a4d7116d04f12463754d1cc9233d634ed0be9326
sykutejnik <sykutejnik@mts.ru>
2021-10-24 08:14:55 +0500

    init project

---
+++ src/main/java/com/example/tdd_spring_sec_with_jwt_and_postgres/TddSpringSecWithJwtAndPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdd_spring_sec_with_jwt_and_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringSecWithJwtAndPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddSpringSecWithJwtAndPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tdd_spring_sec_with_jwt_and_postgres/TddSpringSecWithJwtAndPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdd_spring_sec_with_jwt_and_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringSecWithJwtAndPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
