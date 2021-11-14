ccc9ef3a2e00c87c8628d64ae7bf6278ef4d082e
sykutejnik <sykutejnik@mts.ru>
2021-11-14 10:20:40 +0500

    can get about

--- src/test/java/com/example/tdd_spring_sec_with_jwt_and_postgres/TddSpringSecWithJwtAndPostgresApplicationTests.java
+++ src/test/java/com/example/tdd_spring_sec_with_jwt_and_postgres/TddSpringSecWithJwtAndPostgresApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.tdd_spring_sec_with_jwt_and_postgres;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class TddSpringSecWithJwtAndPostgresApplicationTests {
 
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .build();
+
+    }
+
     @Test
-    void contextLoads() {
+    void canGetAbout() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/about"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("About users!"));
+
     }
 
 }
