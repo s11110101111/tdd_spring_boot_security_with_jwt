6f05bef5629bc9e69dade7e524d469c290921f5d
sykutejnik <sykutejnik@mts.ru>
2021-11-14 12:56:14 +0500

    all green test

---
+++ src/test/java/com/example/tdd_spring_sec_with_jwt_and_postgres/configs/SecurityFilterConfig.java
@@ -0,0 +1,29 @@
+package com.example.tdd_spring_sec_with_jwt_and_postgres.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ */
+@EnableWebSecurity
+public class SecurityFilterConfig {
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .httpBasic(Customizer.withDefaults())
+            .authorizeRequests(authz ->
+                authz
+                    .antMatchers("/api/about").permitAll()
+                    .anyRequest().authenticated()
+
+            );
+
+            return http.build();
+    }
+
+
+}
